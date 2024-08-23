f20b2e61debb889e0ad2d7c6a53a26bda7ef9fd2
abdullah12q <abdullahelsiefy12@gmail.com>
2024-08-23 22:23:15 +0300

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
+from markupsafe import escape
 import db
 import utils
 from flask_limiter import Limiter
@@ -14,6 +15,38 @@ limiter = Limiter(
 default_limits=["50 per minute"], storage_uri="memory://"
 )
 
+@app.route("/")
+def index():
+  return render_template("products.html")
+
+@app.route('/signup', methods=['GET', 'POST'])
+@limiter.limit("10 per minute")
+def sign_up():
+  if request.method == 'POST':
+    first_name = escape(request.form["first_name"])
+    last_name = escape(request.form["last_name"])
+    username = escape(request.form['username'])
+    password = request.form['password']
+
+    if not utils.is_strong_password(password):
+      flash(
+        "Sorry You Entered A Weak Password Please Choose A Stronger One", "danger"
+      )
+      return render_template('signup.html')
+
+    user = db.get_user(connection, username)
+    if user:
+      flash(
+        "Username Already Exists. Please Choose A Different Username.", "danger"
+      )
+      return render_template('signup.html')
+    
+    else:
+      db.add_user(connection,first_name,last_name, username, password)
+      return redirect(url_for('login'))
+
+  return render_template('signup.html')
+
 @app.route('/login', methods=['GET', 'POST'])
 @limiter.limit("10 per minute")
 def login():
@@ -25,52 +58,49 @@ def login():
     
     if db.seed_admin_user(username, password):
       session["username"] = username
-      return render_template("restaurant.html", restaurants=db.get_all_restaurants(connection) , username=session['username'])
+      return render_template("products.html", products=db.get_all_products(connection) , username=session['username'])
 
     if user:
       if utils.is_password_match(password, user[4]):
         session['username'] = user[3]
-        return redirect(url_for('index'))
+        return redirect(url_for('products'))
       else:
-        flash("Password dose not match", "danger")
+        flash("Password does not match", "danger")
         return render_template('login.html')
 
-  else:
-    flash("Invalid username", "danger")
-    return render_template('login.html')
+    else:
+      flash("Invalid username", "danger")
+      return render_template('login.html')
 
   return render_template('login.html')
 
-@app.route('/signup', methods=['GET', 'POST'])
-@limiter.limit("10 per minute")
-def sign_up():
-  if request.method == 'POST':
-    first_name = request.form["first_name"]
-    last_name = request.form["last_name"]
-    username = request.form['username']
-    password = request.form['password']
-    if not utils.is_strong_password(password):
-      flash(
-        "Sorry You Entered a weak Password Please Choose a stronger one", "danger"
-      )
-      return render_template('signup.html')
+@app.route('/home', methods=['GET', 'POST'])
+def products():
+  if 'username' in session:
+    if request.method == 'POST':
+      return render_template('products.html', products = db.get_all_products(connection), username = session['username'])
+  return redirect(url_for(login))
 
-    user = db.get_user(connection, username)
-    if user:
-      flash(
-        "Username already exists. Please choose a different username.", "danger"
-      )
-      return render_template('signup.html')
-    
-    else:
-      db.add_user(connection, username, password , "images.png")
-      return redirect(url_for('login'))
+# @app.route('/addproduct', methods=['GET', 'POST'])
+# def add_product():
 
-  return render_template('signup.html')
 
-@app.route("/")
-def index():
-  return render_template("index.html")
+# @app.route('/product/id=<product_id>')
+# def get_product(product_id):
+
+
+@app.route('/delete-product/id=<product_id>')
+def delete_product(product_id):
+  db.delete_product(connection, product_id)
+  return redirect(url_for('products'))
+
+@app.route("/logout")
+def logout():
+  session.pop("username", None)
+  return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
+  db.init_db(connection)
+  db.init_product(connection)
   app.run(debug=True)
--- db.py
+++ db.py
@@ -4,6 +4,10 @@ def connect_to_database(name='database.db'):
 	import sqlite3
 	return sqlite3.connect(name, check_same_thread=False)
 
+
+# --------------------------------- DATABASE FOR USERS ----------------------------------
+
+
 def init_db(connection):
 	cursor = connection.cursor()
 
@@ -37,7 +41,7 @@ def add_user(connection,first_name,last_name,username,password):
 def get_user(connection,username):
     cursor = connection.cursor()
     query = '''SELECT * FROM users WHERE username =?'''
-    cursor.execute(query, (username))
+    cursor.execute(query, (username,))
     return cursor.fetchone()
 
 
@@ -48,95 +52,109 @@ def get_all_users(connection):
 	return cursor.fetchall()
 
 
+# --------------------------------- DATABASE FOR PRODUCTS ----------------------------------
 
 
-def init_restaurant(connection):
+def init_product(connection):
     cursor = connection.cursor()
 
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS restaurants (
+        CREATE TABLE IF NOT EXISTS products (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title TEXT NOT NULL UNIQUE,
-            description TEXT,
+            name TEXT NOT NULL UNIQUE,
+            price INTEGER NOT NULL,
             image_url TEXT
-           
         )
     ''')
 
     connection.commit()
-    
-    
 
-def add_restaurant(connection, title, description, image_url=None):
+
+def add_product(connection, name, price ,image_url=None):
     cursor = connection.cursor()
-    query = '''INSERT INTO restaurants ( title, description, image_url) VALUES (?, ?, ?)'''
-    cursor.execute(query, ( title, description, image_url))
+    query = '''INSERT INTO products ( name, price, image_url) VALUES (?, ?, ?)'''
+    cursor.execute(query, ( name, price, image_url ))
     connection.commit()
 
-def get_restaurant(connection, restaurant_id):
+def get_product(connection, product_id):
     cursor = connection.cursor()
-    query = '''SELECT * FROM restaurants WHERE id = ?'''
-    cursor.execute(query, (restaurant_id,))
+    query = '''SELECT * FROM products WHERE id = ?'''
+    cursor.execute(query, (product_id,))
     return cursor.fetchone()
 
 
-def get_all_restaurants(connection):
+def get_all_products(connection):
     cursor = connection.cursor()
-    query = '''SELECT * FROM restaurants'''
+    query = '''SELECT * FROM products'''
     cursor.execute(query)
     return cursor.fetchall()
 
-def init_reviews(connection):
-    cursor = connection.cursor()
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS reviews (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            user_id INTEGER NOT NULL,
-            restaurant_id INTEGER NOT NULL,
-            rating INTEGER NOT NULL,
-            review TEXT NOT NULL,
-            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (user_id) REFERENCES users (id),
-            FOREIGN KEY (restaurant_id) REFERENCES restaurants (id)
-           
-        )
-    ''')
-
-    connection.commit()
-
-def add_review(connection, user_id, restaurant_id, rating, review):
-    cursor = connection.cursor()
-    query = '''INSERT INTO reviews (user_id, restaurant_id, rating, review) VALUES (?, ?, ?, ?)'''
-    cursor.execute(query, (user_id, restaurant_id, rating, review))
-    connection.commit()
-
-def get_reviews_for_restaurant(connection, restaurant_id):
+def delete_product(connection, product_id):
     cursor = connection.cursor()
     query = '''
-        SELECT  users.first_name, users.last_name, reviews.review, reviews.rating, reviews.timestamp
-        FROM reviews
-        JOIN users ON reviews.user_id = users.id
-        WHERE reviews.restaurant_id = ?
-    '''
-    cursor.execute(query, (restaurant_id,))
-    return cursor.fetchall()
-
-def search_restaurants(connection, searchkey):
-    cursor = connection.cursor()
-    query = '''
-        SELECT * FROM restaurants
-        WHERE title like '%' || (?) || '%' or description like '%' || (?) || '%'
-    '''
-    cursor.execute(query, (searchkey,searchkey))
-    return cursor.fetchall()
-
-
-def RemoveRestaurant(connection, id):
-    cursor = connection.cursor()
-    query = '''
-        DELETE FROM restaurants
+        DELETE FROM products
         WHERE id = (?)
     '''
-    cursor.execute(query, (id,))
+    cursor.execute(query, (product_id,))
     connection.commit()
+
+
+# --------------------------------- DATABASE FOR BUYING ----------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def init_reviews(connection):
+#     cursor = connection.cursor()
+
+#     cursor.execute('''
+#         CREATE TABLE IF NOT EXISTS reviews (
+#             id INTEGER PRIMARY KEY AUTOINCREMENT,
+#             user_id INTEGER NOT NULL,
+#             restaurant_id INTEGER NOT NULL,
+#             rating INTEGER NOT NULL,
+#             review TEXT NOT NULL,
+#             timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+#             FOREIGN KEY (user_id) REFERENCES users (id),
+#             FOREIGN KEY (restaurant_id) REFERENCES restaurants (id)
+#         )
+#     ''')
+
+#     connection.commit()
+
+# def add_review(connection, user_id, restaurant_id, rating, review):
+#     cursor = connection.cursor()
+#     query = '''INSERT INTO reviews (user_id, restaurant_id, rating, review) VALUES (?, ?, ?, ?)'''
+#     cursor.execute(query, (user_id, restaurant_id, rating, review))
+#     connection.commit()
+
+# def get_reviews_for_restaurant(connection, restaurant_id):
+#     cursor = connection.cursor()
+#     query = '''
+#         SELECT  users.first_name, users.last_name, reviews.review, reviews.rating, reviews.timestamp
+#         FROM reviews
+#         JOIN users ON reviews.user_id = users.id
+#         WHERE reviews.restaurant_id = ?
+#     '''
+#     cursor.execute(query, (restaurant_id,))
+#     return cursor.fetchall()
+
+# def search_restaurants(connection, searchkey):
+#     cursor = connection.cursor()
+#     query = '''
+#         SELECT * FROM restaurants
+#         WHERE title like '%' || (?) || '%' or description like '%' || (?) || '%'
+#     '''
+#     cursor.execute(query, (searchkey,searchkey))
+#     return cursor.fetchall()
